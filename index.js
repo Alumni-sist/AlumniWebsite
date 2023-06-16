9a541c21e3a85a06594ce8d143b4fe07302e9288
Suryam2048 <suryamsourya8@gmail.com>
2023-06-16 14:40:42 +0530

    slide images

--- index.js
+++ index.js
@@ -9,3 +9,28 @@ viewProfileLinks.forEach(link => {
     alert(`You clicked on the profile of ${alumniName}`);
   });
 });
+
+/* Slideshow */
+document.addEventListener("DOMContentLoaded", function(event) {
+  var slides = document.querySelectorAll(".slide");
+  var prevButton = document.querySelector(".prev");
+  var nextButton = document.querySelector(".next");
+  var currentSlide = 0;
+  var slideInterval = setInterval(nextSlide, 3000);
+
+  function nextSlide() {
+    slides[currentSlide].style.display = "none";
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].style.display = "block";
+  }
+
+  prevButton.addEventListener("click", function() {
+    slides[currentSlide].style.display = "none";
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    slides[currentSlide].style.display = "block";
+  });
+
+  nextButton.addEventListener("click", nextSlide);
+});
+
+/*slide show end */
