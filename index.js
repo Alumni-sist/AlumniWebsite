1014e0b19b2595fa47d10a195523f98983223c02
abhi <abhishekbajpai680@gmail.com>
2023-06-16 16:28:32 +0530

    add: Landing Page

--- index.js
+++ index.js
@@ -1,36 +1,97 @@
-// Get all the "View Profile" links
-const viewProfileLinks = document.querySelectorAll('a[href="#"]');
-
-// Add click event listener to each link
-viewProfileLinks.forEach(link => {
-  link.addEventListener('click', (event) => {
-    event.preventDefault();
-    const alumniName = link.parentNode.querySelector('h2').innerText;
-    alert(`You clicked on the profile of ${alumniName}`);
+document.addEventListener("DOMContentLoaded", () => {
+  const inputField = document.getElementById("input");
+  inputField.addEventListener("keydown", (e) => {
+    if (e.code === "Enter") {
+      let input = inputField.value;
+      inputField.value = "";
+      output(input);
+    }
   });
 });
 
-/* Slideshow */
-document.addEventListener("DOMContentLoaded", function(event) {
-  var slides = document.querySelectorAll(".slide");
-  var prevButton = document.querySelector(".prev");
-  var nextButton = document.querySelector(".next");
-  var currentSlide = 0;
-  var slideInterval = setInterval(nextSlide, 3000);
+function output(input) {
+  let product;
 
-  function nextSlide() {
-    slides[currentSlide].style.display = "none";
-    currentSlide = (currentSlide + 1) % slides.length;
-    slides[currentSlide].style.display = "block";
+  // Regex remove non word/space chars
+  // Trim trailing whitespce
+  // Remove digits - not sure if this is best
+  // But solves problem of entering something like 'hi1'
+
+  let text = input.toLowerCase().replace(/[^\w\s]/gi, "").replace(/[\d]/gi, "").trim();
+  text = text
+    .replace(/ a /g, " ")   // 'tell me a story' -> 'tell me story'
+    .replace(/i feel /g, "")
+    .replace(/whats/g, "what is")
+    .replace(/please /g, "")
+    .replace(/ please/g, "")
+    .replace(/r u/g, "are you");
+
+  if (compare(prompts, replies, text)) { 
+    // Search for exact match in `prompts`
+    product = compare(prompts, replies, text);
+  } else if (text.match(/thank/gi)) {
+    product = "You're welcome!"
+  } else if (text.match(/(corona|covid|virus)/gi)) {
+    // If no match, check if message contains `coronavirus`
+    product = coronavirus[Math.floor(Math.random() * coronavirus.length)];
+  } else {
+    // If all else fails: random alternative
+    product = alternative[Math.floor(Math.random() * alternative.length)];
   }
 
-  prevButton.addEventListener("click", function() {
-    slides[currentSlide].style.display = "none";
-    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
-    slides[currentSlide].style.display = "block";
-  });
+  // Update DOM
+  addChat(input, product);
+}
 
-  nextButton.addEventListener("click", nextSlide);
-});
+function compare(promptsArray, repliesArray, string) {
+  let reply;
+  let replyFound = false;
+  for (let x = 0; x < promptsArray.length; x++) {
+    for (let y = 0; y < promptsArray[x].length; y++) {
+      if (promptsArray[x][y] === string) {
+        let replies = repliesArray[x];
+        reply = replies[Math.floor(Math.random() * replies.length)];
+        replyFound = true;
+        // Stop inner loop when input value matches prompts
+        break;
+      }
+    }
+    if (replyFound) {
+      // Stop outer loop when reply is found instead of interating through the entire array
+      break;
+    }
+  }
+  return reply;
+}
 
-/*slide show end */
+function addChat(input, product) {
+  const messagesContainer = document.getElementById("messages");
+
+  let userDiv = document.createElement("div");
+  userDiv.id = "user";
+  userDiv.className = "user response";
+  userDiv.innerHTML = `<img src="user.png" class="avatar"><span>${input}</span>`;
+  messagesContainer.appendChild(userDiv);
+
+  let botDiv = document.createElement("div");
+  let botImg = document.createElement("img");
+  let botText = document.createElement("span");
+  botDiv.id = "bot";
+  botImg.src = "bot-mini.png";
+  botImg.className = "avatar";
+  botDiv.className = "bot response";
+  botText.innerText = "Typing...";
+  botDiv.appendChild(botText);
+  botDiv.appendChild(botImg);
+  messagesContainer.appendChild(botDiv);
+  // Keep messages at most recent
+  messagesContainer.scrollTop = messagesContainer.scrollHeight - messagesContainer.clientHeight;
+
+  // Fake delay to seem "real"
+  setTimeout(() => {
+    botText.innerText = `${product}`;
+    textToSpeech(product)
+  }, 2000
+  )
+
+}
